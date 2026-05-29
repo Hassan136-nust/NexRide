c0918315e722dc96d09074d1bc517bdafc21edf6
Hassan136-nust <hjamal.bscs24seecs@seecs.edu.pk>
2026-05-29 18:15:54 +0500

    Error Fixed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function HeroSection() {
   return (
-    <div>HeroSection</div>
+    <div className='relative min-h-screen w-full overflow-hidden'>
+        <div className='absolute top-0 left-0 w-full h-full bg-cover bg-center' style={{backgroundImage: "url('/heroImage.jpg')"}}>
+
+        </div>
+    </div>
   )
 }
 
--- src/components/PublicHome.tsx
+++ src/components/PublicHome.tsx
@@ -1,11 +1,13 @@
+'use client'
 import React from 'react'
+import { useState } from 'react'
 import HeroSection from './HeroSection'
 import VehicleSection from './VehicleSection'
 import AuthModal from './AuthModal'
 
 function PublicHome() {
 
-const [authOpen , setAuthOpen] = React.useState(false)
+const [authOpen , setAuthOpen] = useState(false)
 
   return (
     <>
